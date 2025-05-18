a7110a2aae4ba05f9171f16674478e03059bee29
pmqueiroz <pedroqueiroz593@gmail.com>
2025-05-18 13:27:59 -0300

    fix: export objectType on lib

--- src/lib/lib.zig
+++ src/lib/lib.zig
@@ -4,3 +4,4 @@ pub const repository = @import("repository.zig");
 pub const file = @import("file.zig");
 pub const compress = @import("compress.zig");
 pub const Record = @import("record.zig").Record;
+pub const objectType = @import("object-type.zig");
